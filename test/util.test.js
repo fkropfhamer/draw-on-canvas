9d5c9b54740c7be76150194b7933d9cd47cd8393
fabian <fabian.kropfhamer@gmail.com>
2020-09-03 20:32:36 +0200

    added tests

--- test/util.test.js
+++ test/util.test.js
@@ -1,7 +1,10 @@
-import {chunkArray} from '../src/util';
+import { chunkArray } from '../src/util';
 
 describe('util', () => {
-    test('', () => {
-        expect(chunkArray([], 0)).Equal([]);
+    test('chunkArray', () => {
+        expect(chunkArray([], 0)).toEqual([]);
+        expect(chunkArray([1, 2, 3], 1)).toEqual([[1], [2], [3]]);
+        expect(chunkArray([1, 2, 3, 4], 3)).toEqual([[1, 2, 3], [4]]);
+        expect(chunkArray(['a', 'b', 'c'], 1)).toEqual([['a'], ['b'], ['c']]);
     })
 });
