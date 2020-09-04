1c8ec769358300ff13e5f4834a9d6b0afbd95b70
fkropfhamer <fabian.kropfhamer@sblum.de>
2020-09-04 13:35:25 +0200

    added tests

--- test/util.test.js
+++ test/util.test.js
@@ -1,4 +1,4 @@
-import { chunkArray } from '../src/util';
+import { chunkArray, downloadURI } from '../src/util';
 
 describe('util', () => {
     test('chunkArray', () => {
@@ -6,5 +6,28 @@ describe('util', () => {
         expect(chunkArray([1, 2, 3], 1)).toEqual([[1], [2], [3]]);
         expect(chunkArray([1, 2, 3, 4], 3)).toEqual([[1, 2, 3], [4]]);
         expect(chunkArray(['a', 'b', 'c'], 1)).toEqual([['a'], ['b'], ['c']]);
+    });
+
+    test('donwloadURI', () => {
+        const mockLink = { click: jest.fn() };
+        document.createElement = jest.fn(() => mockLink);
+        document.body.appendChild = jest.fn();
+        document.body.removeChild = jest.fn();
+
+        downloadURI('test', 'test.txt');
+
+        expect(document.createElement).toHaveBeenCalledTimes(1);
+        expect(document.createElement).toHaveBeenCalledWith('a');
+
+        expect(mockLink.download).toBe('test.txt');
+        expect(mockLink.href).toBe('test');
+
+        expect(document.body.appendChild).toHaveBeenCalledTimes(1);
+        expect(document.body.appendChild).toHaveBeenCalledWith(mockLink);
+        
+        expect(mockLink.click).toHaveBeenCalledTimes(1);
+
+        expect(document.body.removeChild).toHaveBeenCalledTimes(1);
+        expect(document.body.removeChild).toHaveBeenCalledWith(mockLink);
     })
 });
